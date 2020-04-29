9da1e5b6964c83daada58eeec320e24aaf0156ab
gecmenow <gector456@gmail.com>
2020-04-29 21:25:51 +0300

    fix

--- TechnoTent/Cookies/Cookie.cs
+++ TechnoTent/Cookies/Cookie.cs
@@ -1,28 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ExceptionServices;
 using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
 
 namespace TechnoTent.Cookies
 {
     public class Cookie
     {
+        [HandleProcessCorruptedStateExceptions]
         public static string CheckLanguageCookie()
         {
             var language = "ru";
 
-            try
-            {
-                HttpCookie cookie = HttpContext.Current.Request.Cookies["lang"];
+            HttpCookie cookie = null;
 
-                if (cookie != null)
+            var temp = HttpContext.Current;
+
+            if (temp != null)
+            {
+                try
+                {
+                    cookie = temp.Request.Cookies["lang"];
+                }
+                catch(Exception)
+                { }
+            }
+            
+            if (cookie != null)
                     language = cookie.Value;   // если куки уже установлено, то обновляем значение
-            }
-            catch(Exception)
-            {
-
-            }
-
+            
             return language;
         }
     }
